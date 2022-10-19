21d5fc5354878d59c88216b7f0bda38e21d2e77c
Ganiyukolapoabdulrasheed <116133921+Ganiyukolapoabdulrasheed@users.noreply.github.com>
2022-10-19 22:46:24 +0100

    #

---
+++ udp-client.py
@@ -0,0 +1,33 @@
+import socket
+
+def targethost():
+    try:
+        target_host=int(input('target host : '))
+        return target_host
+    except ValueError as error:
+        print(f"You made a/an {error}, No strings are allowed")
+        return targethost()
+
+def targetport():
+    try:
+        target_port = int(input("target port"))
+        return target_port
+    except ValueError as error:
+        print(f"You made a/an {error}, No strings are allowed ")
+        return targetport()
+
+
+# Getting the target host and the target port 
+target_host = targethost()  
+target_port = targetport()
+
+
+# creating a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# sending data
+data = input("send : ")
+client.sendto(data, (target_host, target_port))
+
+# recieving data
+data, addr = client.recvfrom(4096)
